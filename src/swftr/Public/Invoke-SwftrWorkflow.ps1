62c61d2fc7123142ef82202e6635574da564a0e1
John Bruckler <jpbruckler@gmail.com>
2024-10-22 14:32:48 -0400

    checkpoint

---
+++ src/swftr/Public/Invoke-SwftrWorkflow.ps1
@@ -0,0 +1,23 @@
+function Invoke-SwftrWorkflow {
+    <#
+    .SYNOPSIS
+    Short description
+
+    .DESCRIPTION
+    Long description
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    General notes
+    #>
+    [CmdletBinding()]
+    param()
+
+    begin {}
+
+    process {}
+
+    end {}
+}
